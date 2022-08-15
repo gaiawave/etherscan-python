0f8a796430009cdc19e6593cfaa2a34573e40132
gaiawave <101229419+gaiawave@users.noreply.github.com>
2022-08-15 16:38:47 -0700

    Update __init__.py
    Added modules.logs

--- etherscan/__init__.py
+++ etherscan/__init__.py
@@ -4,6 +4,7 @@ from .modules.blocks import Blocks as blocks
 from .modules.contracts import Contracts as contracts
 from .modules.gastracker import GasTracker as gastracker
 from .modules.pro import Pro as pro
+from .modules.logs import Logs as logs
 from .modules.proxy import Proxy as proxy
 from .modules.stats import Stats as stats
 from .modules.tokens import Tokens as tokens
